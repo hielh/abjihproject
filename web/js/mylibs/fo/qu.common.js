f69ef3b69a70d32a1bb50c9da70176f1b32c3454
abjih <abderrahman.jihal@gmail.com>
2011-07-30 02:24:59 +0100

    notification

--- web/js/mylibs/fo/qu.common.js
+++ web/js/mylibs/fo/qu.common.js
@@ -18,7 +18,6 @@ qu.common.execute = function(){
  */
 qu.common.displayNotice = function(data, titleElement, typeNotice){
   
-  
   if (data != null)
   {
     var divObject = jQuery(data).closest('.show-notice');
