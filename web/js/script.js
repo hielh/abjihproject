cfccc6d11302d96afa45f09e124e4fc07a35446e
abjih <abderrahman.jihal@gmail.com>
2011-07-30 01:51:33 +0100

    show spinner on load

--- web/js/script.js
+++ web/js/script.js
@@ -1,21 +1,18 @@
 jQuery(document).ready(function(){
 
-  
+    $.ajaxSetup({
+	beforeSend: function(){
+	    qu.common.showLoader();
+	},
+	complete: function(data){
+	    qu.common.hideLoader();
+	    qu.common.displayNotice(data.responseText);
+	}
+    });
 
-  $.ajaxSetup({
-    start: function(data){
-      alert(1);
-    },
-    complete: function(data){
-      qu.common.hideLoader();
-      qu.common.displayNotice(data.responseText);
-      
-    }
-  });
-
-//action sur la playlist
-  qu.common.execute();
-  qu.player.execute();
+    //action sur la playlist
+    qu.common.execute();
+    qu.player.execute();
 
 });
 
