6b87f867a5451d13c5d764398ff289de8b58fe7d
abjih <abderrahman.jihal@gmail.com>
2011-08-07 15:50:19 +0000

    modification playlist

---
+++ apps/backend/modules/track/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/trackGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/trackGeneratorHelper.class.php';
+
+/**
+ * track actions.
+ *
+ * @package    addakirine
+ * @subpackage track
+ * @author     Abderrahman JIHAL
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class trackActions extends autoTrackActions
+{
+}
---
+++ apps/backend/modules/track/lib/trackGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * track module configuration.
+ *
+ * @package    addakirine
+ * @subpackage track
+ * @author     Abderrahman JIHAL
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class trackGeneratorConfiguration extends BaseTrackGeneratorConfiguration
+{
+}
