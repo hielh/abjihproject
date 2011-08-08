70cb899e955612c04306db6570adda4051155d0a
abjih <abderrahman.jihal@gmail.com>
2011-08-08 19:59:49 +0000

    menu

--- apps/frontend/modules/home/templates/_nav.php
+++ apps/frontend/modules/home/templates/_nav.php
@@ -1,13 +1,13 @@
 <div id="nav">
     <nav>
 	<ul>
-	    <li><a href="#" class="home">home</a></li>
-	    <li><a href="#" class="about">home</a></li>
-	    <li><a href="#" class="done">home</a></li>
-	    <li><a href="#" class="blog">home</a></li>
-	    <li><a href="#" class="ads">home</a></li>
-	    <li><a href="#" class="pub">home</a></li>
-	    <li><a href="#" class="contact">home</a></li>
+	    <li class="high"><a href="#" class="home"><span>home</span></a></li>
+	    <li class="high"><a href="#" class="about"><span>home</span></a></li>
+	    <li class="high"><a href="#" class="done"><span>home</span></a></li>
+	    <li class="high"><a href="#" class="blog"><span>home</span></a></li>
+	    <li class="high"><a href="#" class="ads"><span>home</span></a></li>
+	    <li class="high"><a href="#" class="pub"><span>home</span></a></li>
+	    <li class="high"><a href="#" class="contact"><span>home</span></a></li>
 	</ul>
     </nav>
 </div>
--- web/js/script.js
+++ web/js/script.js
@@ -16,3 +16,39 @@ jQuery(document).ready(function(){
 
 });
 
+
+$(function () {
+  // IE6 doesn't handle the fade effect very well - so we'll stick with
+  // the default non JavaScript version if that is the user's browser.
+  if ($.browser.msie && $.browser.version < 7) return;
+  
+  $('#nav ul li')
+  
+    // remove the 'highlight' class from the li therefore stripping 
+    // the :hover rule
+    
+    .removeClass('high')
+    // within the context of the li element, find the a elements
+    .find('a')
+    
+    // create our new span.hover and loop through anchor:
+    .append('<span class="hover" />').each(function () {
+      
+      // cache a copy of the span, at the same time changing the opacity
+      // to zero in preparation of the page being loaded
+      var $span = $('> span.hover', this).css('opacity', 0).css('position', 'absolute').css('top', 0);
+      
+      // when the user hovers in and out of the anchor
+      $(this).hover(function () {
+        // on hover
+        
+        // stop any animations currently running, and fade to opacity: 1
+        $span.stop().fadeTo(500, 1);
+      }, function () {
+        // off hover
+        
+        // again, stop any animations currently running, and fade out
+        $span.stop().fadeTo(500, 0);
+      });
+    });
+});
