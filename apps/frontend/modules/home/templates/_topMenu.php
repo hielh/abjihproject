f1eb325cf0d8498523fc712c1679a8ed20bde884
abjih <abderrahman.jihal@gmail.com>
2011-08-08 19:07:01 +0000

    fixbug on header

--- apps/frontend/modules/home/templates/_topMenu.php
+++ apps/frontend/modules/home/templates/_topMenu.php
@@ -1,24 +1,24 @@
-<div class="grid_12 right" id="header_date">
-<span class="th">الجمعة</span> 5 رمضان 1432 هـ الموافق لـ 5 غشت 2011 م					</div>
-					<div class="grid_5" id="header_search">
-						<form>
-							<input type="search" placeholder="search" />
-							<button type="submit"></button>
-						</form>
-					</div>
-					
-					<div class="grid_5" id="header_link">
-						<ul>
-							<li><a href="#" class="signup">signup</a></li>
-							<li><a href="#">login</a></li>
-						</ul>
-					</div>
-					
-					<div class="grid_2" id="header_share">
-						<ul>
-							<li><a href="#" class="facebook">signup</a></li>
-							<li><a href="#" class="rss">login</a></li>
-							<li><a href="#" class="twitter">login</a></li>
-							<div class="clear"></div>
-						</ul>
-					</div>
+<div class="grid_11 right" id="header_date">
+    <span class="th">الجمعة</span> 5 رمضان 1432 هـ الموافق لـ 5 غشت 2011 م					</div>
+<div class="grid_6" id="header_search">
+    <form>
+	<input type="search" placeholder="search" />
+	<button type="submit"></button>
+    </form>
+</div>
+
+<div class="grid_5" id="header_link">
+    <ul>
+	<li><a href="#" class="signup">signup</a></li>
+	<li><a href="#">login</a></li>
+    </ul>
+</div>
+
+<div class="grid_2" id="header_share">
+    <ul>
+	<li><a href="#" class="facebook">signup</a></li>
+	<li><a href="#" class="rss">login</a></li>
+	<li><a href="#" class="twitter">login</a></li>
+	<div class="clear"></div>
+    </ul>
+</div>
