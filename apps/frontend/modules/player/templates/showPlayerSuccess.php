14497aac4f446accd0f74118110624a8ba5756ee
abjih <abderrahman.jihal@gmail.com>
2011-08-10 20:10:43 +0000

    affiche css

--- apps/frontend/modules/player/templates/showPlayerSuccess.php
+++ apps/frontend/modules/player/templates/showPlayerSuccess.php
@@ -1,34 +1,45 @@
 <?php /* @var $player playOwner */ ?>
 
 <div class="play_owner_info">
-    <h2 class="text-lime"><?php echo $player->getName(); ?></h2>
+    <h2 >شاري بن راشد العفاسي</h2>
+    <div class="image">
+	<img src="/images/kzabri.jpg">
+    </div>
     <div class="play_owner_info_text">
-	<div class="image"><img src="/uploads/<?php echo $player->getPicture(); ?> " /></div>
 	<div class="info">
 	    <p>
 		<strong>الاسم:</strong>
-	      				 مشاري بن راشد بن غريب بن محمد بن راشد العفاسي.
+									مشاري بن راشد بن غريب بن محمد بن راشد العفاسي.
 	    </p>
 	    <p>
 		<strong>الاسم:</strong>
-	      				 مشاري بن راشد بن غريب بن محمد بن راشد العفاسي.
+									مشاري بن راشد بن غريب بن محمد بن راشد العفاسي.
 	    </p>
 	    <p>
 		<strong>الاسم:</strong>
-	      				 مشاري بن راشد بن غريب بن محمد بن راشد العفاسي.
+									مشاري بن راشد بن غريب بن محمد بن راشد العفاسي.
 	    </p>
+	    <div class="play_owner_info_links">
+		<ul>
+		    <li><a href="#" class="share">شــارك الصفحـــة مع أصدقــــاءك</a></li>
+		    <li><a href="#" class="favorite">أضف هذا المقرئ إلى المفضلة</a></li>
+		    <li><a href="#" class="ilike">أضف هذا المقرئ إلى المفضلة</a></li>
+		</ul>
+	    </div>
 	</div>
-
 
 	<div class="clear"></div>
     </div>
-
-    <div class="play_owner_info_links">
+    <div class="clear"></div>
+    <div class="play_owner_info_onglet">
 	<ul>
-	    <li class="share"><a href="#">شــارك الصفحـــة مع أصدقــــاءك</a></li>
-	    <li class="favorite"><a href="#">أضف هذا المقرئ إلى المفضلة</a></li>
+	    <li><a href="#" class="quran_active">hohoho</a></li>
+	    <li><a href="#" class="about_active">hohoho</a></li>
+	    <li><a href="#" class="anashid_active">hohoho</a></li>
+	    <li><a href="#" class="pic_active">hohoho</a></li>
+	    <li><a href="#" class="video_active">hohoho</a></li>
+	    <li><a href="#" class="doc_active">hohoho</a></li>
 	</ul>
-	
     </div>
 </div>
 
