a3e4bbd03e85b503d49fca4a2698df02cd3e2611
abjih <abderrahman.jihal@gmail.com>
2011-08-11 13:45:48 +0000

    ajout fixture

--- apps/frontend/modules/player/templates/_playLists.php
+++ apps/frontend/modules/player/templates/_playLists.php
@@ -1,4 +1,4 @@
-<?php $i = 0; ?>
+
 <div class="play_listing">
     <div id="breadcrumbs">
 	<ul>
@@ -9,11 +9,25 @@
 
     </div>
     <?php foreach ($playLists as $playList): ?>
+      <?php $i = 0; ?>
+      <?php $j = 0; ?>
+      <?php $k = 0; ?>
         <div class="play_sublisting">
 	    <h2><span class="colapsed"></span><?php echo $playList->getTitle(); ?></h2>
 	    <?php foreach ($playList->getTracks() as $Track): ?>
 		<?php $class = ($i % 2) ? 'alpha' : 'omega'; ?>
-		<div class="grid_9  <?php echo $class; ?> play_sublisting_item">
+		<?php 
+        if ($j==0 or $j==1)
+        {
+          $odd = '';
+          
+        }
+        else
+        {
+          $odd = 'odd';
+        }
+        ?>
+		<div class="grid_9  <?php echo $class.' '.$odd; ?> play_sublisting_item">
 		    <div>
 			<a href="#" class="track grid_5"><?php echo $Track->getName(); ?></a>
 			<ul class="play_sublisting_actions">
@@ -25,7 +39,20 @@
 			</ul>
 		    </div>
 		</div>
+        <?php 
+        if ($j==3)
+        {
+          $j = 0;
+          
+        }
+        else{
+        
+          $j++;
+        }
+        ?>
 		<?php $i++; ?>
+		<?php  ?>
+		<?php $k++; ?>
 	    <?php endforeach; ?>
 
     	<div class="clear"></div>
